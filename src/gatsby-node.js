40d17ab84c50905c4c10b5c8146ec2eaf29662d2
Kieran White <kieran@bluesquareoffices.com>
2020-03-26 11:28:30 +0000

    Modifying headers test.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -21,7 +21,8 @@ exports.sourceNodes = async ({
     method: 'GET',
     cache: 'no-cache',
     headers: {
-      'Origin': 'https://www.yell.com/'
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+      'Host': 'www.yell.com'
     }
   };
 
